06f48cb88a2652da339752dc840f3c540c3ed6ee
Evandro Pomatti <evandro.pomatti@gmail.com>
2022-10-09 16:14:04 -0300

    destroy lifecycle

--- apps/delivery/src/main/java/io/pomatti/bookstore/delivery/services/DeliveryConsumer.java
+++ apps/delivery/src/main/java/io/pomatti/bookstore/delivery/services/DeliveryConsumer.java
@@ -3,11 +3,14 @@ package io.pomatti.bookstore.delivery.services;
 import java.time.LocalDateTime;
 import java.util.function.Consumer;
 
+import javax.annotation.PreDestroy;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
 
 import com.azure.messaging.servicebus.ServiceBusClientBuilder;
@@ -16,6 +19,7 @@ import com.azure.messaging.servicebus.ServiceBusProcessorClient;
 import com.azure.messaging.servicebus.ServiceBusReceivedMessageContext;
 
 @Service
+@Scope("singleton")
 public class DeliveryConsumer {
 
   Logger logger = LoggerFactory.getLogger(DeliveryConsumer.class);
@@ -34,6 +38,8 @@ public class DeliveryConsumer {
 
   private final static String ORDERS_QUEUE = "orders";
 
+  private static ServiceBusProcessorClient processorClient;
+
   public void start() {
     Consumer<ServiceBusReceivedMessageContext> processMessage = messageContext -> {
       try {
@@ -49,13 +55,14 @@ public class DeliveryConsumer {
     };
 
     Consumer<ServiceBusErrorContext> processError = errorContext -> {
-      System.err.println("Error occurred while receiving message: " + errorContext.getException());
+      logger.error("Error occurred while receiving message", errorContext.getException());
     };
 
-    ServiceBusProcessorClient processorClient = new ServiceBusClientBuilder()
+    processorClient = new ServiceBusClientBuilder()
         .connectionString(connectionString)
         .processor()
         .maxConcurrentCalls(maxConcurrentCalls)
+        // .maxAutoLockRenewDuration(Duration.ofSeconds(60))
         .prefetchCount(prefetchCount)
         .queueName(ORDERS_QUEUE)
         .processMessage(processMessage)
@@ -64,6 +71,17 @@ public class DeliveryConsumer {
         .buildProcessorClient();
 
     processorClient.start();
+  }
+
+  @PreDestroy
+  public void close() {
+    logger.info("Closing Service Bus Processor");
+    if (processorClient != null) {
+      processorClient.close();
+      logger.info("Closed Service Bus Processor");
+    } else {
+      logger.info("Service Bus processor was null");
+    }
   }
 
   protected Delivery buildDelivery(Long orderId) {
